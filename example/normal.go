8293aeb997d5dda3510fa8d791110b9a61182da8
ota42y <ota42y@gmail.com>
2015-12-20 12:19:13 +0900

    support map data

--- example/normal.go
+++ example/normal.go
@@ -7,7 +7,13 @@ import (
 type Normal struct {
 	Date    time.Time   `toml:"date"`
 	Dates   []time.Time `toml:"dates"`
+	Group   Group       `toml:"group"`
 	Name    string      `toml:"name"`
 	Num     int64       `toml:"num"`
 	Numbers []int64     `toml:"numbers"`
+}
+
+type Group struct {
+	Name   string `toml:"name"`
+	School string `toml:"school"`
 }
